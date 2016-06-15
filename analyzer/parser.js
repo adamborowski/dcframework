a11d88e80e4dcf1681192a3597a87caff0ca4ab7
Adam Borowski <aborowski@atlassian.com>
2016-06-15 20:23:15 +0200

    initial tester state

--- analyzer/parser.js
+++ analyzer/parser.js
@@ -1,6 +1,6 @@
 var Finder = require('./file-finder');
 var finder = new Finder();
-finder.getFiles('/Users/aborowski/Documents/dcframework-reports/node1', loadJson);
+finder.getFiles('/Users/aborowski/Documents/dcframework-reports/node1_2', loadJson);
 var fileReg = /(.*)\.run\.(\d)\.json/;
 
 function mean(a, b) {
@@ -27,15 +27,23 @@ function loadJson(files) {
 
     var counter=0;
     console.log('case\t problem size\t num nodes\t tx\t randomThreshold\t o1\t o2\t num threads\t  computation time');
-    cases.sort((a, b)=>a.problemSize - b.problemSize);
+    var caseArray = [];
     for (var caseName in cases) {
-        var _case = cases[caseName];
+        caseArray.push(cases[caseName]);
+    }
+    caseArray = caseArray.sort((a, b)=>Number(a.runs[0].args.endParameter) - Number(b.runs[0].args.endParameter));
+    for (var _case of caseArray) {
         _case.args = _case.runs[0].args;
         var args = _case.args;
         _case.problemSize = args.endParameter;
         _case.result = _case.runs[0].result;
         _case.numNodes = Object.keys(_case.runs[0].nodeStatistics).length;
-        _case.computationTime = mean(_case.runs[0].nodeStatistics[0].computationTime, _case.runs[1].nodeStatistics[0].computationTime);
+        if (_case.runs.length > 1) {
+            _case.computationTime = mean(_case.runs[0].nodeStatistics[0].computationTime, _case.runs[1].nodeStatistics[0].computationTime);
+        }
+        else {
+            _case.computationTime = _case.runs[0].nodeStatistics[0].computationTime;
+        }
         console.log(`${counter++}\t ${_case.problemSize}\t ${_case.numNodes}\t ${args.maxThreshold}\t ${args.randomThreshold}\t ${args.optimizeShortReturn}\t ${args.optimizeInitialDistribution}\t ${args.numThreads}\t ${_case.computationTime}`)
     }
 
